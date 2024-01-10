ce9c3d6611a9c94c861fd46536cd0719071e863b
Ray Kao <raykao@gmail.com>
2024-01-10 11:35:58 -0500

    add: market research semantic plugin and corresponding function

--- function_app.py
+++ function_app.py
@@ -9,29 +9,11 @@ from semantic_kernel.core_skills import WebSearchEngineSkill
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
-@app.route(route="evaluate_kid_friendliness")
-async def evaluate_kid_friendliness(req: func.HttpRequest) -> func.HttpResponse:
-    """
-    An Azure Function triggerable by HTTP request. It accepts a JSON body
-    that includes a general location. The input is passed to Semantic Kernel
-    and any configured plugins before determining a response back to the
-    user.
-    params:
-        req : func.HttpRequest
-            The originating HTTP request triggering the function including a
-            JSON body with a `location` parameter (e.g., 
-            `{ "location": "Jackson Heights Queens, NY" }`)
-    returns:
-        func.HttpResponse
-            An HTTP response with a JSON value representing the response from
-            the LLM after processing through the Semantic Kernel plugins (e.g.,
-            `{ "result": "Kid-friendliness rating for Jackson Heights: 4" }`)
-    raises:
-        an error if required environment variables have not been set (through
-        the application settings)
-    """
-    # Assign the location in which we're interested to determine kid friendliness
+@app.route(route="generate_market_research")
+async def generate_market_research(req: func.HttpRequest) -> func.HttpResponse:
     location = req.params.get('location')
+    property_type = req.params.get('property_type')
+
     if not location:
         try:
             req_body = req.get_json()
@@ -44,21 +26,24 @@ async def evaluate_kid_friendliness(req: func.HttpRequest) -> func.HttpResponse:
             "Please pass a `location` on the query string or in the request body",
             status_code=400
         )
-
-    logging.info(f'Running the Kid Friendliness Evaluator for {location}')
-
-    logging.info('Checking for Azure OpenAI and Bing Search settings...')
+    
+    if not property_type:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            property_type = req_body.get('property_type')
+    if not property_type:
+        return func.HttpResponse(
+            "Please pass a `property_type` on the query string or in the request body",
+            status_code=400
+        )
 
     aoai_deployment_name = os.environ["AZURE_OPEN_AI__CHAT_COMPLETION_DEPLOYMENT_NAME"]
     aoai_endpoint = os.environ["AZURE_OPEN_AI__ENDPOINT"]
     aoai_api_key = os.environ["AZURE_OPEN_AI__API_KEY"]
-    bing_api_key = os.environ["AZURE_BING_SEARCH__API_KEY"]
 
-    logging.info('Initializing the Semantic Kernel...')
-
-    # Setup logging for semantic_kernel.kernel to DEBUG for all the details
-    logging.getLogger("kernel").setLevel(logging.DEBUG)
-    # Initialize the kernel
     kernel = sk.Kernel()
     # Add Azure OpenAI for chat completions
     kernel.add_chat_service(
@@ -70,32 +55,45 @@ async def evaluate_kid_friendliness(req: func.HttpRequest) -> func.HttpResponse:
         ),
     )
 
-    # Load built-in plugins
-    search_engine_connector = BingConnector(api_key = bing_api_key)
-    kernel.import_skill(WebSearchEngineSkill(search_engine_connector), "WebSearch")
-        
-    logging.info('Loading custom plugins...')
+    market_research_questions =	[
+        f"Write a sentence about {location} and {property_type} leasing",
+        f"How to get to and from {location}",
+        f"Popular neighborhoods in {location} for {property_type} leasing",
+        f"Write a short paragraph about {location}",
+        f"Are {property_type} rents going to increase in {location}?", 
+        f"Write a short meta description for {property_type} for lease in {location}",
+        f"Write an optimised meta description for {property_type} for lease in {location}",
+	]
 
-    # Load custom plugins
+    history = ""
+    context = sk.ContextVariables()
+    answers = []
+
     plugins_directory = "./plugins"
-    kid_friendliness_plugin = kernel.import_semantic_skill_from_directory(
-        plugins_directory, "KidFriendlinessPlugin"
+    cw_plugins = kernel.import_semantic_skill_from_directory(
+        plugins_directory, "CushmanWakefieldPlugins"
     )
 
-    logging.info('Processing the request...')
 
-    # Run!
-    variables = sk.ContextVariables()
-    variables["input"] = location
-    variables["searchQuery"] = f'places in {location}'
-    variables["num_results"] = '4'
-    result = await kernel.run_async(
-        kid_friendliness_plugin["Evaluate"],
-        input_vars = variables,
-    )
+    for question in market_research_questions:
+        history = history + "\nUser: " + question
 
-    logging.info('Returning the result as JSON.')
+        context["history"] = history
+        context["input"] = question
+        answer = await kernel.run_async(
+            cw_plugins["AskMarketResearchQuestion"], 
+            input_vars = context
+        )
+
+        answers.append({
+            question: answer.result
+        })
+
+        history = history + "\nAssistant: " + answer.result
+
+    print(json.dumps(answers))
+
     return func.HttpResponse(
-        json.dumps(result.result),
+        json.dumps(answers),
         mimetype="application/json",
     )
